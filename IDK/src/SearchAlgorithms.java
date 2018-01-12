340ece358269a4f237215b12c7e969eef5e12c9f
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-12 09:08:39 -0500

    IT WORKS!

--- IDK/src/SearchAlgorithms.java
+++ IDK/src/SearchAlgorithms.java
@@ -9,11 +9,11 @@ public class SearchAlgorithms
 		System.out.println(binSearch(test1,3));
 		System.out.println(linSearch(test1,3));
 		
-		System.out.println(binSearch(test1,23));
-		System.out.println(linSearch(test1,23));
+		System.out.println(binSearch(test2,23));
+		System.out.println(linSearch(test2,23));
 		
-		System.out.println(binSearch(test1,25));
-		System.out.println(linSearch(test1,25));
+		System.out.println(binSearch(test2,25));
+		System.out.println(linSearch(test2,25));
 		
 		System.out.println(binSearch(test1,10));
 		System.out.println(linSearch(test1,1));
@@ -28,11 +28,29 @@ public class SearchAlgorithms
 			{
 				return(x);
 			}
-			return (-1);
 		}
+		return (-1);
 	}
 	public static int binSearch(int[] a, int q)
 	{
-		
+		int l = 0;
+		int r = a.length - 1;
+		while (l <= r)
+		{
+			int mid = (l+r)/2;
+			if (q == a[mid])
+			{
+				return (mid);
+			}
+			else if(q < a[mid])
+			{
+				r = mid-1;
+			}
+			else 
+			{
+				l = mid + 1;
+			}
+		}
+		return (-1);
 	}
 }
